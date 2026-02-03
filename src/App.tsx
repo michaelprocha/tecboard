b09f520ce0dea0501cb590f396a6a940f64e6600
Michael Rocha <michaelrocha9722@gmail.com>
2026-02-03 11:29:13 -0300

    add size in Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import AddIcon from './assets/icons/add_circle.svg?react'
+import Button from "./components/Button"
 function App() {
 
   return (
     <>
-    
+    <Button size={'md'} icon={AddIcon} variant={'primary'}>Adicionar evento</Button>
     </>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ import Icon from "./Icon";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const buttonVariant = cva(
-	["flex", "items-center", "justify-center", "rounded-md", "px-4", "py-2", "gap-2.5", "text-base", "font-semibold"],
+	["flex", "items-center", "justify-center", "rounded-md", "px-4", "py-2", "gap-2.5", "text-base", "font-semibold", 'min-w-fit'],
 	{
 		variants: {
 			variant: {
@@ -13,20 +13,28 @@ const buttonVariant = cva(
 				tertiary: ["bg-medium-gray", "text-white", "border", "border-white"],
 				Quaternary: ["bg-graphite", "text-white"]
 			},
+			size: {
+				sm: ['min-h-9'],
+				md: ['min-h-11.5']
+			},
 		},
+		defaultVariants: {
+			variant: "primary",
+			size: 'sm'
+		}
 	},
 );
 
-interface ButtonProps extends VariantProps<typeof buttonVariant>{
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariant>{
   children: React.ReactNode;
   icon?: React.FC<React.ComponentProps<"svg">>;
   className?: string;
 }
 
-function Button({children, icon, variant, className, ...props}: ButtonProps) {
+function Button({children, icon, size, variant, className, ...props}: ButtonProps) {
 	return (
-		<Text as={"button"} className={buttonVariant({variant, className})}>
-      {!icon ? "" : (<Icon svg={icon} variant={variant === 'primary' ? 'primary' : 'secondary'} className={'w-6 h-7.5'} { ...props }/>)}
+		<Text as={"button"} className={buttonVariant({variant, size, className})} {...props}>
+      		{!icon ? "" : (<Icon svg={icon} variant={variant === 'primary' ? 'primary' : 'secondary'} className={'w-6 h-7.5'}/>)}
 			{children}
 		</Text>
 	);
